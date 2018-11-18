e7a03c585314acaaa110e527d2fa541a37f879e6
Federico Pereiro <fpereiro@gmail.com>
2018-11-18 12:38:01 +0100

    Fix deployment.

--- server/deploy.sh
+++ server/deploy.sh
@@ -6,7 +6,11 @@ TAR="model.tar.gz"
 cd ../.. && tar --exclude="$FOLDER/*.swp" -czvf $TAR $FOLDER
 scp $TAR $HOST:
 ssh $HOST tar xzvf $TAR
-echo "main = node server $1" | ssh $HOST "cat >> $FOLDER/mongroup.conf"
-ssh $HOST "cd $FOLDER && npm i --no-save && mg restart"
+
+ssh $HOST "cd $FOLDER/modules/ARM && npm i --no-save"
+ssh $HOST "cd $FOLDER/modules/APM && npm i --no-save"
+ssh $HOST "cd $FOLDER/modules/DIM && npm i --no-save"
+ssh $HOST "cd $FOLDER/modules/DPM && npm i --no-save"
+ssh $HOST "cd $FOLDER/server && npm i --no-save && mg restart"
 ssh $HOST rm $TAR
 rm $TAR
