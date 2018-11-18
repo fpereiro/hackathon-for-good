6d6ee61ae704a2e183502f6d48ad65700811c099
Federico Pereiro <fpereiro@gmail.com>
2018-11-18 09:12:35 +0100

    Deployment/provisioning scripts.

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+HOST="USER@IP"
+
+FOLDER="hackathon-for-good"
+TAR="model.tar.gz"
+
+cd .. && tar --exclude="acpic/*.swp" -czvf $TAR $FOLDER
+scp $TAR $HOST:
+ssh $HOST tar xzvf $TAR
+echo "main = node server $1" | ssh $HOST "cat >> $FOLDER/mongroup.conf"
+ssh $HOST "cd $FOLDER && npm i --no-save && mg restart"
+ssh $HOST rm $TAR
+rm $TAR
